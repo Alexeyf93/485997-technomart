88d26a18d9a7c969e0fb515ac1b72d64a1f0d7b1
Unknown <alexeyf93@mail.ru>
2018-09-05 20:58:42 +0500

    Завершение верстки 2

--- js/popap-buy.js
+++ js/popap-buy.js
@@ -1,22 +1,28 @@
-	var product = document.querySelector(".catalog-buy");
-    var popupbuy = document.querySelector(".modal-buy");
-    var closebuy = document.querySelector(".modal-buy-close");
+	for (var product = document.querySelectorAll(".catalog-buy"), 
+      	popupbuy = document.querySelector(".modal-buy"), 
+      	closebuy = document.querySelector(".modal-buy-close"),
+      	continue_shopping = document.querySelector(".right-button"), 
+      	i = 0; i < product.length; i++)
+    	product[i].addEventListener("click", function(t) {
+        t.preventDefault(),
+        popupbuy.classList.add("modal-buy-show")
+    });
 
-    product.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popupbuy.classList.add("modal-buy-show");
-      });
-
-      closebuy.addEventListener("click", function (evt) {
+    closebuy.addEventListener("click", function (evt) {
         evt.preventDefault();
         popupbuy.classList.remove("modal-buy-show");
-      });
+    });
 
-      window.addEventListener("keydown", function (evt) {
+    continue_shopping.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popupbuy.classList.remove("modal-buy-show");
+    });
+
+    window.addEventListener("keydown", function (evt) {
         if (evt.keyCode === 27) {
             evt.preventDefault();
           if (popupbuy.classList.contains("modal-buy-show")) {
             popupbuy.classList.remove("modal-buy-show");
             }
         }
-      });
+    });
